1333c595f5525c4cda3af30052a7ed20cc1fe5bd
LeLwy <Louis.Chancioux@gmail.com>
2023-05-02 12:23:51 +0200

    Mise en place du formulaire de contact

--- assets/js/index.js
+++ assets/js/index.js
@@ -21,4 +21,44 @@ window.addEventListener('DOMContentLoaded', function(){
             homeBanner.classList.toggle('inactive');
         })
     }
+
+    /***** CONTACT FORM VERIFICATION *****/
+
+    function checkContactForm() {
+
+        let contactForm = document.getElementById('contact-form');
+    
+        contactForm.addEventListener('submit', function(e) {
+    
+            let contactName = document.getElementById('contactName').value.trim();
+            let contactEmail = document.getElementById('contactEmail').value.trim();
+            let contactMessage = document.getElementById('contactMessage').value.trim();
+    
+            if (contactName.length < 2) {
+                alert('Votre prénom doit faire au moins 2 caractères.');
+                e.preventDefault();
+                return;
+            }
+    
+            if (contactEmail === '') {
+                alert('Veuillez renseigner votre email.');
+                e.preventDefault();
+                return;
+            }
+    
+            if (contactMessage.length < 15) {
+                alert('Votre message doit faire au moins 15 caractères.');
+                e.preventDefault();
+                return;
+            }
+    
+            // Si toutes les vérifications sont passées, on peut soumettre le formulaire
+            alert('Votre message a bien été envoyé, nous reviendrons vers vous dans les plus brefs délais');
+            contactForm.submit();
+        });
+    
+    }
+    
+    let contactForm = document.getElementById('contact-form');
+    contactForm.addEventListener('submit', checkContactForm);
 })
